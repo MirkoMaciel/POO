dd2439f26acdaa5c49cea05f52e0cf24cb62bca9
MirkoMaciel <mirko.maciell@gmail.com>
2022-08-05 13:26:43 -0300

    Agg Punto 10
    Sin terminar

--- Practico 1/Punto 10/src/principal.java
+++ Practico 1/Punto 10/src/principal.java
@@ -1,28 +1,60 @@
-import java.util.Random;
+import java.io.IOException;
+import java.util.Random; //libreria random
 import java.util.Scanner;
 
 public class principal {
 
 	public static void main(String[] args) {
-		Random ran = new Random();
-		Integer valor1 , valor2 , dato;
-		Scanner in = new Scanner(System.in);
+		Random ran = new Random(); //Creo una variable de tipo ran asignada a la libreria
+		Integer valor1 , valor2; //Creo variables de tipo entero(obj)
+		Integer victoria = 0 , derrota = 0;
+		char rta; 
+		int dato;
+		Scanner in = new Scanner(System.in);   
 		
 		do {
-			System.out.print("Primer numero: ");
-			valor1 = ran.nextInt(100);
-			System.out.print("Segundo numero: ");
+			
+	        /* try { //linea para borrar la consola
+	            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+	        } catch (IOException | InterruptedException e) {
+	       
+	        } */
+	      
+			
+		do { 			
+			valor1 = ran.nextInt(100);			
 			valor2 = ran.nextInt(100);				
-		}while (valor1 == valor2);
+		}while (valor1 == valor2); //Genero los valores aleatorios y no finaliza el bucle hasta que sean distintos
 		
-/*		System.out.println("APUESTA  - EL NUMERO DE MAYOR VALOR GANA - ELIJA ");
-		System.out.print("Elija 1 o 2: "); 
-		dato = in.nextInt();
+		System.out.print("A que numero desea apostar? Numero 1 o Numero 2: ");
+		dato=in.nextInt();
+		System.out.println("Numero 1: "+valor1);
+		System.out.println("Numero 2: "+valor2);
+						
+		switch (dato) {
+
+		case 1 : if (valor1>valor2) {
+			System.out.println("Ganador. ");
+			victoria = victoria + 1;
+			break;
+		}
 		
-		System.out.print("Los numeros son: "+valor1+" "+valor2);
-		if (dato == 1 && valor1 > valor2) {
-			System.out.print("Ganador");
-		}else System.out.print("Perdedor"); */
+		case 2 : if (valor2>valor1) {
+			System.out.print("Ganador. ");
+			victoria = victoria + 1;
+			break;
+		}
+		
+		default : System.out.println("Perdedor"); 
+					derrota = derrota + 1;
+		}
+		
+		System.out.println("Desea volver a jugar? S/N");
+		rta=in.next().toUpperCase().charAt(0);  //Se obtiene el primer caracter del String Mayusculisado
+		
+		}while(rta == 'S');
+		System.out.println("Victorias "+victoria);
+		System.out.println("Derrota "+derrota);
 	}
 
 }
