95ccca07a718240f4183c99b1a932828392eb959
MirkoMaciel <mirko.maciell@gmail.com>
2022-08-08 18:20:52 -0300

    Actualización Punto 10
    problemas con el Try ( manejo de excepciones) para la consistencia de datos.
    Ingresar un entero entre 1 y 2.
    No otro tipo de dato y no fuera de ese rango

--- Practico 1/Punto 10/src/principal.java
+++ Practico 1/Punto 10/src/principal.java
@@ -1,36 +1,43 @@
-import java.io.IOException;
+//import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Random; //libreria random
 import java.util.Scanner;
 
 public class principal {
 
+	
 	public static void main(String[] args) {
 		Random ran = new Random(); //Creo una variable de tipo ran asignada a la libreria
 		Integer valor1 , valor2; //Creo variables de tipo entero(obj)
 		Integer victoria = 0 , derrota = 0;
 		char rta; 
-		int dato;
+		Integer dato= 0;
 		Scanner in = new Scanner(System.in);   
 		
 		do {
-			
-	        /* try { //linea para borrar la consola
-	            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-	        } catch (IOException | InterruptedException e) {
-	       
-	        } */
-	      
 			
 		do { 			
 			valor1 = ran.nextInt(100);			
 			valor2 = ran.nextInt(100);				
 		}while (valor1 == valor2); //Genero los valores aleatorios y no finaliza el bucle hasta que sean distintos
+							
 		
+		do {
 		System.out.print("A que numero desea apostar? Numero 1 o Numero 2: ");
-		dato=in.nextInt();
+		try {
+			dato = in.nextInt();	
+		}catch (InputMismatchException e) {
+			//e.printStackTrace(); //Mostrar que hay en la pila de errores
+			System.out.println("No me diste un valor numerico. ");
+			
+		}
+		
+		}while (dato == 1);
+		
 		System.out.println("Numero 1: "+valor1);
 		System.out.println("Numero 2: "+valor2);
-						
+		
+		
 		switch (dato) {
 
 		case 1 : if (valor1>valor2) {
