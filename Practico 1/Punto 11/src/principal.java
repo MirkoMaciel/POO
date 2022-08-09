477120894d19c4acc5f743b6ff5ae1335bbd099f
MirkoMaciel <mirko.maciell@gmail.com>
2022-08-08 22:46:51 -0300

    Agg punto 11
    Finalizado.

--- Practico 1/Punto 11/src/principal.java
+++ Practico 1/Punto 11/src/principal.java
@@ -1,9 +1,61 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Random ran = new Random();
+		Scanner in = new Scanner (System.in);
+		Integer vecEnteros [] = new Integer [6] ;
+		Integer vecUser [] = new Integer [6];
+		int i, x;
+		Integer aciertos = 0;
+		Boolean bol = true;
+		Character rta;
+		
+		
+		while (bol == true){
+		for (i = 0 ; i<6 ; i++) {
+			vecEnteros[i] = ran.nextInt(1,50); 
+		}
+		
+
+		System.out.println("Ingrese los número que desea jugar. ");
+		System.out.println("Sin importar orden, en una seguidilla. ");
+		for (i = 0 ; i<6 ; i++) {
+			vecUser[i] = in.nextInt(); 
+		}
+		
+		
+		System.out.print("Combinacion ganadora: ");
+		for (i = 0 ; i < 6 ;i++) {
+			System.out.print(vecEnteros[i]+", ");
+		} 
+		
+		System.out.println("");
+		
+		System.out.print("Numero elegidos: ");
+		for (i = 0 ; i < 6 ;i++) {
+			System.out.print(vecUser[i]+", ");
+		} 
+	
+		for (i = 0 ; i < 6 ;i++) {
+			for (x = 0 ; x < 6 ;x++) { 	
+				if (vecEnteros[i] == vecUser [x]) {
+					aciertos++;
+				}
+			}
+		} 
+		System.out.println("");
+		System.out.println("Aciertos: "+aciertos);
+		
+		System.out.println("Desea volver a jugar? S/N");
+			rta = in.next().toUpperCase().charAt(0);
+			if (rta != 'S') {
+				bol = false;
+			}
+		}
 
 	}
-
 }
