88465ccdc849c6e22497a6e09661e38464072b5d
MirkoMaciel <mirko.maciell@gmail.com>
2022-08-10 11:13:54 -0300

    Agg punto 4

--- Practico 2/Punto 4/src/Alumno.java
+++ Practico 2/Punto 4/src/Alumno.java
@@ -5,9 +5,46 @@ public class Alumno {
 	private String apellido;
 	private int dni;
 	
-	public Alumno(String nombre, String apellido, int dni) {
-	
+
+	public Alumno(String nombre, String apellido, int dni) { //COnstructor
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
 	}
+
+	//getters and setters de los atributos.
+	
+	public String getNombre() { 
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + "]";
+	}
+	
+	
+	
 	
 }
 
--- Practico 2/Punto 4/src/principal.java
+++ Practico 2/Punto 4/src/principal.java
@@ -4,8 +4,10 @@ public class principal {
 	public static void main(String[] args) {
 
 
-		Alumno alumno1 = new Alumno();
-		alumno1.setNombre("Alejandro");
+		Alumno alumno1 = new Alumno("Mirko", "Maciel", 40205842);
+		System.out.println(alumno1);
+		
+		/* alumno1.setNombre("Alejandro");
 		alumno1.setApellido("Rojas");
 		alumno1.setDni(11111111);
 		Alumno alumno2 = new Alumno("Martin", "Rosales");
@@ -14,7 +16,10 @@ public class principal {
 		alumno1.getDni());
 		System.out.println("Nombre y apellido: " + alumno2.getNombreYApellido() + " - DNI: " +
 		alumno2.getDni());
-		
+		*/
 
 
 }
+}
+
+	//1 elimine los paquetes y separe la clase alumno
