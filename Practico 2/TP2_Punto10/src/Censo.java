e1f7cc493f582a8aca4a0b5e4545fec94f19cad1
MirkoMaciel <mirko.maciell@gmail.com>
2022-08-13 20:26:39 -0300

    Agg punto 10

--- Practico 2/TP2_Punto10/src/Censo.java
+++ Practico 2/TP2_Punto10/src/Censo.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Censo {
 	
-	Persona p = new Persona()
+	Persona p = new Persona();
 	Familia f = new Familia();
 	
 	ArrayList<Familia> grupoFamiliar = new ArrayList<Familia>();
--- Practico 2/TP2_Punto10/src/Main.java
+++ Practico 2/TP2_Punto10/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+
 //import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		Persona p = new Persona();
+		Familia f = new Familia();
+		
+		ArrayList<Familia> grupoFamiliar = new ArrayList<Familia>();
 
 	}
 }
