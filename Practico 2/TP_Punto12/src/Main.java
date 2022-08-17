912664d9167f6bdc3eaf947e8badb6c7ff53601e
MirkoMaciel <110473143+MirkoMaciel@users.noreply.github.com>
2022-08-17 19:49:04 -0300

    Agg actualizacion

--- Practico 2/TP_Punto12/src/Main.java
+++ Practico 2/TP_Punto12/src/Main.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,15 +11,24 @@ public class Main {
 		Persona p = new Persona("Mirko", "Maciel", LocalDate.of(2001, 3, 9));  
 		Persona p3 = new Persona("Claudia", "Casas", LocalDate.of(1973, 12, 14));
 		Persona p1 = new Persona();
-
+		Character rta;
+		Scanner in = new Scanner(System.in);
 		
 		System.out.println("-Empresa-");
 		empresa.setNombre("Mirkos company");
 		empresa.setDireccion("Km3 - Foyel 61");
 			empleados.add(p);
 			empleados.add(p3);
-			empresa.generarEmpleado(p1);
-			empleados.add(p1);	
+			
+			do {
+			
+		    empresa.generarEmpleado(p1);
+		    empleados.add(p1);	
+		    System.out.println("Otro empleado? S/N");
+			rta = in.next().toUpperCase().charAt(0);
+			}while(rta == 'S');
+			
+			//empleados.add(p1);	
 		System.out.println(empresa.getNombre());
 		System.out.println(empresa.getDireccion());
 		System.out.print("Empleados: ");System.out.println(empleados+"\n");
